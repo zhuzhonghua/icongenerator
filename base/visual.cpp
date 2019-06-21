ff655dc57a53b94189facc95d6707c3b6a34b218
zhonghua <zhonghua@bogon.pwrd.com>
2019-06-21 19:18:56 +0800

    pos size

--- base/visual.cpp
+++ base/visual.cpp
@@ -2,7 +2,24 @@
 
 #include "visual.h"
 
+void Visual::setPos(int x, int y)
+{
+	_bound.x = x;
+	_bound.y = y;
+}
+
+void Visual::setSize(int w, int h)
+{
+	_bound.w = w;
+	_bound.h = h;
+}
+
 void Visual::setBound(int x, int y, int w, int h)
 {
 	_bound = Utils::rect(x,y,w,h);
+}
+
+void Visual::setBound(const Rect& bound)
+{
+	_bound = bound;
 }
--- base/visual.h
+++ base/visual.h
@@ -3,9 +3,11 @@
 #include "gizmo.h"
 
 class Visual:public Gizmo{
-public:	
+public:
+	void setPos(int x, int y);
+	void setSize(int w, int h);
 	void setBound(int x, int y, int w, int h);
-
+	void setBound(const Rect& bound);
 protected:
 	Rect _bound;
 };
