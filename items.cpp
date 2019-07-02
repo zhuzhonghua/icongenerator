04abab22623c9214761479cb524f16053a29a702
zhonghua <zhonghua@bogon.pwrd.com>
2019-07-02 16:56:56 +0800

    items setpos

--- items.cpp
+++ items.cpp
@@ -5,13 +5,22 @@
 
 Items::Items()
 {
-	Image* item = new Image("data/head/1.png");
-	item->setSize(100, 100);
-	_items.push_back(item);
+	for(int i=0;i<21;i++)
+	{
+		Image* item = new Image("data/head/1.png");
+		item->setSize(100, 100);
+		_items.push_back(item);
+	}
 }
 
 Items::~Items()
 {
+	for(int i=0; i<_items.size(); i++)
+	{
+		delete _items[i];
+	}
+	
+	_items.clear();
 }
 
 void Items::draw()
@@ -24,5 +33,14 @@ void Items::draw()
 
 void Items::setPos(int x, int y)
 {
-	//_item->setPos(x,y);
+	int tempX=x, tempY=y;
+	for(int i=0; i<_items.size(); i++)
+	{
+		_items[i]->setPos(tempX, tempY);
+		tempX += 100;
+		if((i+1)%5==0){
+			tempX = x;
+			tempY += 100;
+		}
+	}
 }
